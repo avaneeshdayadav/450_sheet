46d8ba63f3554a10b73121c35c348501690116a1
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-04-02 16:46:01 +0530

    Create 24_Rotate_DDL_by_N_nodes_counter_clockwise.cpp

---
+++ LinkedList/CPP/24_Rotate_DDL_by_N_nodes_counter_clockwise.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	public:
+		char data;
+		Node* next;
+		Node* pre;
+	Node(int data)
+	{
+		this->data=data;
+		pre=NULL;
+		next=NULL;
+	}
+};
+
+void rotateByN(Node* &head, int pos)
+{
+	// return without any changes if position is 0.
+	if(pos==0) return;
+
+	// Finding last node.
+	Node* temp=head;
+	while(temp->next!=NULL)
+	{
+		temp=temp->next;
+	}
+	// making the list circular.
+	temp->next=head;
+	head->pre=temp;
+
+	// move head and temp by the given position.
+	int count=1;
+	while(count<=pos)
+	{
+		head=head->next;
+		temp=temp->next;
+		count++;
+	}
+
+	// now again make list un-circular.
+	temp->next=NULL;
+	head->pre=NULL;
+}
